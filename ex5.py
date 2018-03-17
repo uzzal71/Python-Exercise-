d80febd50149c99ea8ff093120e9e4b44738c513
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-17 23:28:58 +0600

    More Variables and Printing

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("my name is %s" % my_name)
+print("are you %d years old" % my_age)
+print("my height %d and my weight %d" % (my_height, my_weight))
+print("My eyes colors %s" % my_eyes)
+print("my teeth colors %s" % my_teeth)
+print("my hair is %s" % my_hair)
+
+print("Age %d , Height %d, and weight %d, total = %d" %(my_age, my_height, my_weight, my_age + my_height + my_weight))
