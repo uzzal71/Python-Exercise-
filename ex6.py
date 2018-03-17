f57e195f023857f007ad984ab0a2d5a93bb6a0f2
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-18 00:01:07 +0600

    Strings and Text

---
+++ ex6.py
@@ -0,0 +1,20 @@
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print(x)
+print(y)
+
+print("I said: %r." % x)
+print("I also said: '%s'." % y)
+
+uzzal = False
+joke_nasir = "Isn't that joke so funny?! %r"
+
+print(joke_nasir % uzzal)
+
+first_name = "Uzzal Kumar.."
+last_name = "Roy"
+
+print(first_name + last_name)
