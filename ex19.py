456235ddb741d22a2356189b02187268f818a267
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-31 23:56:53 +0600

    Function and Variable

---
+++ ex19.py
@@ -0,0 +1,24 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+	print("You have %d cheeses!" % cheese_count)
+	print("You have %d boxes of crackers" % boxes_of_crackers)
+	print("Man that's enough for a party!")
+	print("Get a blanket!\n")
+
+print("First function call\n")
+
+cheese_and_crackers(20, 50)
+
+amount_chesses = 10
+amount_crackers = 50
+
+print("Sceond Function call\n")
+
+cheese_and_crackers(amount_chesses, amount_crackers)
+
+print("thrid function call!\n")
+
+cheese_and_crackers(50+10, 60+8)
+
+print("Four function call\n")
+
+cheese_and_crackers(amount_chesses + 40, amount_crackers + 20)
